97e5ba045ce64bf9ef9d5e46c62f4631618689b9
Riley Durbin <epicman534@gmail.com>
2026-01-13 16:42:32 -0800

    Make tutorial states get saved to a file, and have this be turned off while in the editor

---
+++ GGameUtils/Plugins/GGameUtilsTutorialSystem/Source/GGameUtilsTutorialSystem/Public/TutorialSaveGame.h
@@ -0,0 +1,37 @@
+// Copyright (c) 2025 Guardbrawl Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+
+#include "GameplayTagContainer.h"
+#include "TutorialSaveGame.generated.h"
+
+
+USTRUCT()
+struct GGAMEUTILSTUTORIALSYSTEM_API FTutorialCompletionInfo
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	UPROPERTY(EditAnywhere)
+	int numCompletions = 0;
+};
+/**
+ * Brief: Save game for the completion states and any other info for the GGameUtils Tutorial System
+ */
+UCLASS()
+class GGAMEUTILSTUTORIALSYSTEM_API UTutorialSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere)
+	TMap<FGameplayTag, FTutorialCompletionInfo> mSavedTutorialStates;
+
+	UPROPERTY(EditAnywhere)
+	bool testSaveWorking = false;
+	
+};
