2926ea74ff86b12e5b8ba81000abf5e7ebfe6508
Riley Durbin <epicman534@gmail.com>
2026-01-16 18:12:35 -0800

    Add setting in editor preferences for ignoring persistent tutorial saves in editor

---
+++ GGameUtils/Plugins/GGameUtilsTutorialSystem/Source/GGameUtilsTutorialSystem/Public/TutorialPerUserSettings.h
@@ -0,0 +1,22 @@
+// Copyright (c) 2025 Guardbrawl Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "TutorialPerUserSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS(config = EditorPerProjectUserSettings, meta = (DisplayName = "GGameUtils TutorialSystem", ClassGroup = Plugins))
+class GGAMEUTILSTUTORIALSYSTEM_API UTutorialPerUserSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+public:
+
+	UPROPERTY(EditAnywhere, config, meta=(Tooltip="Whether to ignore saving tutorial states between sessions in editor. On by default, so you can test tutorials each time you play in editor"))
+	bool IgnorePersistentTutorialSavesInEditor = true;
+
+	static const UTutorialPerUserSettings* Get() { return GetDefault<UTutorialPerUserSettings>(); }
+};
