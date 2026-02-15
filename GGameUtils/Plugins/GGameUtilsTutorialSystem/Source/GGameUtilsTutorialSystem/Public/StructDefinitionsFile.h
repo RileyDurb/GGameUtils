9856863a72c8b1f71faa357dcf7da3102a8dd1bc
Riley Durbin <epicman534@gmail.com>
2026-02-15 13:44:53 -0800

    Update some tutorials for screenshots, update all C++ file copyright notices
    All C++ files now have the copyright notice, and have been updated to 2026

--- GGameUtils/Plugins/GGameUtilsTutorialSystem/Source/GGameUtilsTutorialSystem/Public/StructDefinitionsFile.h
+++ GGameUtils/Plugins/GGameUtilsTutorialSystem/Source/GGameUtilsTutorialSystem/Public/StructDefinitionsFile.h
@@ -1,8 +1,9 @@
-// Copyright (c) 2025 Guardbrawl Games
+// Copyright (c) 2026 Guardbrawl Games
 
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/DataTable.h"
 #include "StructDefinitionsFile.generated.h"
 
 USTRUCT(BlueprintType)
@@ -14,9 +15,9 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Text", meta=(Multiline = true))
 	FText tutorialTextOverride;
 
-	// Data table made from type "RichTextStyleRow" That you can use to define the text's font, size and more. Reccomend not doing anything it user leaves this variable left blank
+	// Data table made from type "RichTextStyleRow" that you can use to define the text's font, size and more. Reccomend not doing anything it user leaves this variable left blank
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Text")
-	UDataTable* textStyleDataTable;
+	UDataTable* textStyleDataTable = nullptr;
 
 };
 
