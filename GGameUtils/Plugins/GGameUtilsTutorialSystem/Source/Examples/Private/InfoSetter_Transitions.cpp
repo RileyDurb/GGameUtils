6964980194bd233f66cdb4287e161f2d0b5f4903
Riley Durbin <111633437+RileyDurb@users.noreply.github.com>
2026-07-12 00:44:21 -0700

    C++ changes for new transition option tags

--- GGameUtils/Plugins/GGameUtilsTutorialSystem/Source/Examples/Private/InfoSetter_Transitions.cpp
+++ GGameUtils/Plugins/GGameUtilsTutorialSystem/Source/Examples/Private/InfoSetter_Transitions.cpp
@@ -5,8 +5,14 @@
 
 // Open transitions gameplay tag comments (The rest of the tag definition)
 UE_DEFINE_GAMEPLAY_TAG_COMMENT(GGameUtils_TutorialOpenTransitions_FromLeft, "GGameUtils.TutorialOpenTransitions.FromLeft", "Transition option where the widget flys in from the left");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(GGameUtils_TutorialOpenTransitions_FromRight, "GGameUtils.TutorialOpenTransitions.FromRight", "Transition option where the widget flys in from the right");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(GGameUtils_TutorialOpenTransitions_FromTop, "GGameUtils.TutorialOpenTransitions.FromTop", "Transition option where the widget flys in from the top");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(GGameUtils_TutorialOpenTransitions_FromBottom, "GGameUtils.TutorialOpenTransitions.FromBottom", "Transition option where the widget flys in from the bottom");
 UE_DEFINE_GAMEPLAY_TAG_COMMENT(GGameUtils_TutorialOpenTransitions_FadeIn, "GGameUtils.TutorialOpenTransitions.FadeIn", "Transition option where the widget fades from invisible to visible, right in place");
 
 // Close transitions gameplay tag comments (The rest of the tag definition)
 UE_DEFINE_GAMEPLAY_TAG_COMMENT(GGameUtils_TutorialCloseTransitions_ToLeft, "GGameUtils.TutorialCloseTransitions.ToLeft", "Transition option where the widget flys out to the left");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(GGameUtils_TutorialCloseTransitions_ToRight, "GGameUtils.TutorialCloseTransitions.ToRight", "Transition option where the widget flys out to the right");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(GGameUtils_TutorialCloseTransitions_ToTop, "GGameUtils.TutorialCloseTransitions.ToTop", "Transition option where the widget flys out to the top");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(GGameUtils_TutorialCloseTransitions_ToBottom, "GGameUtils.TutorialCloseTransitions.ToBottom", "Transition option where the widget flys out to the bottom");
 UE_DEFINE_GAMEPLAY_TAG_COMMENT(GGameUtils_TutorialCloseTransitions_FadeOut, "GGameUtils.TutorialCloseTransitions.FadeOut", "Transition option where the widget fades from visible to invisible, right in place");
